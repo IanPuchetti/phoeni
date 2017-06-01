0547b2b879e2eea95442eb20b760a37fe8425895
Ian <ianpuchetti@gmail.com>
2017-05-31 21:50:15 -0300

    q

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 app.use(session({secret:'phoenix-log'}));
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
