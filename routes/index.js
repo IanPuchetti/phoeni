ad5dea00684c0584189a49c742414ad081ae2947
Ian Puchetti <ianpuchetti@gmail.com>
2017-07-17 03:51:12 -0300

    c

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var mongodb = require('mongodb');
 var session = require('express-session');
 var MongoClient = mongodb.MongoClient;
-var uri = 'mongodb://127.0.0.1:27017/phoenix';
+var uri = 'mongodb://192.168.1.5:27017/phoenix';
 var url = 'mongodb://ianpuchetti:5446@cluster0-shard-00-00-5nuxu.mongodb.net:27017,cluster0-shard-00-01-5nuxu.mongodb.net:27017,cluster0-shard-00-02-5nuxu.mongodb.net:27017/phoenix?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 
-/* GET home page. */
+//INICIO
 router.get('/', function(req, res, next) {
   if(req.session.user){
     res.render('inicio', { title: 'Phoenix-Log' , user: req.session.user});
@@ -15,6 +15,7 @@ router.get('/', function(req, res, next) {
 }
 });
 
+//CAMIONES
 router.get('/camiones', function(req, res, next) {
   if(req.session.user){
     res.render('camiones', { title: 'Phoenix-Log' , user: req.session.user});
@@ -23,8 +24,60 @@ router.get('/camiones', function(req, res, next) {
 }
 });
 
+
+//CHOFERES
+router.get('/choferes', function(req, res, next) {
+  if(req.session.user){
+    res.render('choferes', { title: 'Phoenix-Log' , user: req.session.user});
+  }else{
+    res.render('index', { title: 'Phoenix-Log' });
+}
+});
+
+//MIS DATOS
+router.get('/mis-datos', function(req, res, next) {
+  if(req.session.user){
+    res.render('mis-datos', { title: 'Phoenix-Log' , user: req.session.user});
+  }else{
+    res.render('index', { title: 'Phoenix-Log' });
+}
+});
+
+router.post('/mis-datos/datos', function(req, res, next) {
+    MongoClient.connect(url, function (err, db) {
+                if (err) {}else{
+                console.log('connected');
+                var collection = db.collection('users');
+                collection.find({user:req.body.user}).toArray(function (err, result) {
+                if(err){console.log(err);}
+                else if (result.length){res.send(result);}
+                else {console.log('nothing')}
+                db.close();}
+                );
+                }
+                });
+});
+
+
+router.post('/mis-datos/modificar/password', function(req, res, next) {
+    MongoClient.connect(url, function (err, db) {
+                if (err) {}else{
+                console.log('connected');
+                var collection = db.collection('users');
+                collection.update({user:req.body.user},{user:req.body.user, password:req.body.password},function (err, result) {
+                if(err){res.send('error');}
+                else if (result){res.send('ok');}
+                else {res.send('error');}
+                db.close();}
+                );
+                }
+                });
+});
+
+
+//LOGIN
 router.post('/login', function(req, res, next) {
-	MongoClient.connect(url, function (err, db) {
+    MongoClient.connect(url, function (err, db) {
                 if (err) {}else{
                 console.log('connected');
                 var collection = db.collection('users');
@@ -37,6 +90,8 @@ router.post('/login', function(req, res, next) {
                 }
                 });
 });
+
+//LOGOUT
 
 router.get('/logout', function(req, res, next) {
   req.session.destroy();
